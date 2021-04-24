9ed00fe3f913c3b5dedc6cedc5125c82a2af0afb
Therese Lofstrom <b20thelo@student.his.se>
2021-04-24 11:13:52 +0200

    - changed image - added logo - changed texts - added transparent images on top of image left/right - added message for button left/right - added button and images to mainactivity

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewLeft;
+    private TextView textViewRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewLeft = findViewById(R.id.left_text);
+        Button button = findViewById(R.id.left_button);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        textViewLeft.setVisibility(View.VISIBLE);
+                    }
+                });
+
+        textViewRight = findViewById(R.id.right_text);
+        Button button2 = findViewById(R.id.right_button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewRight.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
